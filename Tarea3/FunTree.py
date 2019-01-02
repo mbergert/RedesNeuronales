e47b6e2021f87ea82ece03f4db2c513a53a52fd5
mtrujilloberger <maria.trujillo@ug.uchile.cl>
2019-01-02 02:00:13 -0300

    "Function Tree library"

--- Tarea3/FunTree.py
+++ Tarea3/FunTree.py
@@ -2,28 +2,35 @@ from random import randint, random
 from copy import deepcopy
 
 
-# Clase creada para tener objetos Tree con los que trabajará el algoritmo genético
-class Tree(object):
+# Clase con variables
+class FunTree(object):
     # Init crea un arbol al azar dandole valores aleatoriamente respetando una profundidad máxima
-    def __init__(self, maxdepth, ops, terms):
+    def __init__(self, maxdepth, ops, nterms):
         self.left = None
         self.right = None
         self.data = None
         self.parent = None
         self.ops = ops
-        self.terms = terms
+        self.terms = []
+        # crea los terminales para la creacion
+        for i in range(0, nterms):
+            self.terms.append(randint(0, 100))
         # Creacion, si es 1 uno añade un nuevo tree, sino, termina.
 
         gen = random()
 
         if gen > 0.3 and maxdepth > 0:
             self.data = self.ops[randint(0, len(self.ops) - 1)]
-            self.right = Tree(maxdepth - 1, self.ops, self.terms)
+            self.right = FunTree(maxdepth - 1, self.ops, nterms)
             self.right.parent = self
-            self.left = Tree(maxdepth - 1, self.ops, self.terms)
+            self.left = FunTree(maxdepth - 1, self.ops, nterms)
             self.left.parent = self
         else:
-            self.data = self.terms[randint(0, len(self.terms) - 1)]
+            if random() > 0.5:
+                self.data = str(self.terms[randint(0, len(self.terms) - 1)])
+            else:
+                self.data="x"
+
 
     # Imprime un arbol en forma de operación
 
@@ -34,11 +41,15 @@ class Tree(object):
             return self.data
         return "(" + self.left.__str__() + " " + self.data + " " + self.right.__str__() + ")"
 
-    # Evalua el resultado de un arbol de operaciones
-    def evalTree(self):
-        return eval(self.__str__())
+        # Evalua el resultado de un arbol de operaciones
 
-    # Devuelve la copia de un objeto arbol sin que esté referenciada al objeto orifinal
+    def evalTree(self, xval):
+        tree= self.__str__()
+        tree= tree.replace("x", str(xval))
+        return eval(tree)
+
+        # Devuelve la copia de un objeto arbol sin que esté referenciada al objeto orifinal
+
     def copyTree(self):
         return deepcopy(self)
 
@@ -52,5 +63,9 @@ class Tree(object):
         else:
             return [self]
 
+ops = ['*', '+', '-']
+terms = ["14", "18", "1", "4", "10", "15", "3", "5"]
 
-
+a = FunTree(4, ops, 10)
+print(a)
+print(a.evalTree(5))
